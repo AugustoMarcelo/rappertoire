8f6813f149846b52833bc1edbc11e2544cdb443a
Marcelo <mrclgst10@gmail.com>
2022-06-29 08:25:26 -0300

    refactor: move dropdown to a component and add onSubmit prop

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,81 +1,54 @@
-import { Feather } from '@expo/vector-icons';
 import { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import DropDownPicker from 'react-native-dropdown-picker';
-import { theme } from '../../theme';
+import { View } from 'react-native';
+import { CreateDTO } from '../../storage/IStorage';
 import { getMusicStyles } from '../../utils/getMusicStyles';
+import { BorderlessButton } from '../BorderlessButton';
+import { Dropdown } from '../Dropdown';
 import { Input } from '../Input';
 
-export function Form() {
+interface FormProps {
+  onSubmit: (data: CreateDTO) => void;
+}
+
+export function Form({ onSubmit }: FormProps) {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  const [style, setStyle] = useState('');
   const [items, setItems] = useState(getMusicStyles);
+
+  const [title, setTitle] = useState('');
+  const [number, setNumber] = useState('');
+
+  function onHandlePress() {
+    onSubmit({
+      title,
+      number: parseInt(number),
+      style,
+    });
+
+    setTitle('');
+    setNumber('');
+    setStyle('');
+  }
+
   return (
     <View style={{ marginTop: 36 }}>
-      <Input label="Nome da música" />
-      <Input label="Número" keyboardType="numeric" />
-      <DropDownPicker
+      <Input label="Nome da música" value={title} onChangeText={setTitle} />
+      <Input
+        label="Número"
+        keyboardType="numeric"
+        value={number}
+        onChangeText={setNumber}
+      />
+      <Dropdown
+        label="Estilo musical"
         open={open}
-        value={value}
+        value={style}
         items={items}
         setOpen={setOpen}
-        setValue={setValue}
+        setValue={setStyle}
         setItems={setItems}
-        labelStyle={{
-          fontFamily: theme.fonts.regular,
-          color: theme.colors.black[100],
-          fontSize: 14,
-        }}
-        dropDownDirection="BOTTOM"
-        placeholder="Selecione um estilo"
-        listMode="MODAL"
-        style={{
-          paddingTop: 0,
-          paddingHorizontal: 0,
-          marginBottom: 16,
-          borderWidth: 0,
-          borderBottomWidth: 1,
-          borderColor: theme.colors.gray[200],
-        }}
-        ArrowDownIconComponent={() => (
-          <Feather
-            name="chevron-down"
-            size={24}
-            color={theme.colors.lightBlue[500]}
-          />
-        )}
-        ArrowUpIconComponent={() => (
-          <Feather
-            name="chevron-up"
-            size={24}
-            color={theme.colors.lightBlue[500]}
-          />
-        )}
-        TickIconComponent={() => (
-          <Feather name="check" size={20} color={theme.colors.lightBlue[500]} />
-        )}
-        textStyle={{
-          fontFamily: theme.fonts.regular,
-          color: theme.colors.gray[400],
-          fontSize: 12,
-        }}
       />
-      <TouchableOpacity
-        style={{
-          marginTop: 8,
-          alignSelf: 'flex-end',
-        }}
-        onPress={() => console.log('salvar')}
-      >
-        <Text
-          style={{
-            fontFamily: theme.fonts.bold,
-            color: theme.colors.lightBlue[800],
-          }}
-        >
-          Salvar
-        </Text>
-      </TouchableOpacity>
+      <BorderlessButton label="Salvar" onPress={onHandlePress} />
     </View>
   );
 }
