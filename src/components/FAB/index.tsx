4f8e1b7ddbca2d270a04c0ae9ea886ddc1e08c63
Marcelo <mrclgst10@gmail.com>
2022-07-05 15:10:13 -0300

    feat: add a background-color prop

--- src/components/FAB/index.tsx
+++ src/components/FAB/index.tsx
@@ -1,15 +1,27 @@
 import Feather from '@expo/vector-icons/Feather';
-import { TouchableHighlight, TouchableOpacityProps } from 'react-native';
+import {
+  ColorValue,
+  TouchableHighlight,
+  TouchableOpacityProps,
+} from 'react-native';
 import { theme } from '../../theme';
 import { styles } from './styles';
 
 interface FABProps extends TouchableOpacityProps {
   icon: keyof typeof Feather.glyphMap;
+  backgroundColor?: ColorValue;
 }
 
-export function FAB({ icon, ...rest }: FABProps) {
+export function FAB({ icon, backgroundColor, ...rest }: FABProps) {
   return (
-    <TouchableHighlight {...rest} style={[styles.button, rest.style]}>
+    <TouchableHighlight
+      {...rest}
+      style={[
+        styles.button,
+        backgroundColor ? { backgroundColor: backgroundColor } : {},
+        rest.style,
+      ]}
+    >
       <Feather name={icon} size={24} color={theme.colors.white} />
     </TouchableHighlight>
   );
